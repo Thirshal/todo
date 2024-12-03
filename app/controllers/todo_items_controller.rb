7b574c2fe2c6abffbedc67f568c9c653a4656e95
Thrishalmadasu <thrishalmadasu@gmail.com>
2024-12-03 13:24:15 +0530

    Added the functionality to add a todo list , add items to the todo list and delete the items from the todo list

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -6,6 +6,16 @@ class TodoItemsController < ApplicationController
       redirect_to @todo_list
     end
 
+    def destroy 
+      @todo_item = @todo_list.todo_items.find(params[:id])
+      if @todo_item.destroy
+        flash[:success] = "Deleted Successfully"
+      else
+        flash[:error] = "Could not be deleted"
+      end
+      redirect_to @todo_list
+    end
+
     private 
 
     def set_todo_list
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :todo_lists do
-    resources :todo_items
+    resources :todo_items , only: [:create, :update, :destroy]
   end
 
   root "todo_lists#index"
